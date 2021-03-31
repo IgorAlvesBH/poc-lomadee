bfb21ab5a4446e58ec48b2b9d2b269844dc461cb
Igor Moreira Alves <moreiralves@gmail.com>
2021-03-31 17:51:52 -0300

    Removendo arquivos não utilizados.

--- test/globals.js
+++
@@ -1,9 +0,0 @@
-const { dropDatabase } = require('./database');
-const config = require('../lib/conf');
-const database = require('../lib/database');
-
-before('Sobrescreve a url do banco e incia a conexão do banco', async () => {
-  config.set('MONGODB_URI', `${config.get('MONGODB_URI')}_test`);
-  await database.connect();
-});
-after('Remove a base de dados usada para testes', () => dropDatabase());
